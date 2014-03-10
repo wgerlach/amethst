aade2ea0b9e2d35698b13c13d0df4abf13cfe263
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2014-03-10 11:44:21 -0500

    x

--- lib/AMETHSTAWE.pm
+++ lib/AMETHSTAWE.pm
@@ -388,7 +388,7 @@ sub delete_job {
 	my $job = $awe->delete_jobs('awe' => $awe, 'shock' => $shock , 'clientgroup' => $clientgroup, 'jobs'=> [$job_id]);
 	
 	
-	my $status = $job->{'status'} || "unknown";
+	my $status = $job->{'data'}->{'state'} || "unknown";
 	
 	return $status;
 	
